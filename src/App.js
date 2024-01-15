4b965dc00ac4a4bca2bf91ad0aefe47a429eed3c
yzou235 <yumingzou235@gmail.com>
2024-01-15 00:38:03 -0500

    finished html and css set up in app.js and app.css.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState(''); //initial state
+  const [date, setDate] = useState('');
+  const [description, setDescription] = useState('');
+
+  function addNewTransaction() {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="top-panel">
+        <h1>$400<span>.00</span></h1>
+        <form onSubmit = {addNewTransaction}>
+          <div className='basic'>
+            <input  type="text" 
+                    value={name} 
+                    onChange={ev => setName(ev.target.value)}
+                    placeholder={'item'}/>
+            <input  type="date"
+                    value={date}
+                    onChange = {ev => setDate(ev.target.value)} />
+          </div>
+          <div className='description'>
+            <input  type="text" 
+                    value={description}
+                    onChange = {ev => setDescription(ev.target.value)}
+                    placeholder={'description'} />
+          </div>
+          <button type='submit'>Add New Transaction</button>
+        </form>
+      </div>
+      <div className="bottom-panel">
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">1 New Samsung TV</div>
+              <div className="description">it was time for a new tv</div>
+            </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="date">2022-12-24</div>
+          </div>
+          </div>
+          <div className="transaction">
+            <div className="left">
+              <div className="name">2 Gig job new web</div>
+              <div className="description">it was time for a new tv</div>
+            </div>
+            <div className="right">
+              <div className="price green">+$400</div>
+              <div className="date">2022-12-24</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
